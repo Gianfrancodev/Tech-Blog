b7f33c82fb99fd9921d582f9a651fe9ecdf71cd0
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-02-01 22:05:00 -0700

    models and js code

---
+++ public/js/profile.js
@@ -0,0 +1,42 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const title = document.querySelector('#title').value.trim();
+    const content = document.querySelector('#content').value.trim();
+  
+    if (title && content) {
+      const response = await fetch(`/api/blogPosts`, {
+        method: 'POST',
+        body: JSON.stringify({ title, content }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create project');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogPosts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete Post');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-project-form')
+    .addEventListener('submit', newFormHandler);
